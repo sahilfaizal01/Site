902cced255e4a3c1e893f128f04012e04779e97d
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2022-10-07 21:04:06 +0530

    Update main.php

--- main.php
+++ main.php
@@ -6,6 +6,7 @@ if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $email= $_POST['email'];
     $phone= $_POST['phone'];
+    $subject= $_POST['subject'];
     $body= $_POST['body'];
     $headers = 'From: '.$email . "\r\n";
 
@@ -13,7 +14,7 @@ if(isset($_POST['submit'])){
     $body = "name : ".$name. "\r\n" .
     		"Phone : ".$phone. "\r\n" .
     		"Message : " . $body;
-    if(mail($to, $body , $headers)){
+    if(mail($to, $subject ,$body , $headers)){
         echo '<script type ="text/JavaScript">';  
         echo 'alert("Mail Sent!")';
         echo '</script>';  
